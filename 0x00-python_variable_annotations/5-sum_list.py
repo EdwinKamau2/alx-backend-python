b71443d97273ebc9656013ccb9d9d84dac13d9a6
EdwinKamau2 <edwinkamau2@gmail.com>
2024-01-11 14:36:26 +0000

    sum list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Sum list.
+'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''This Computes the sum of a list of floating-point numbers.
+    '''
+    return float(sum(input_list))
